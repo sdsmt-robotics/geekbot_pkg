d5c9bbe0b77f728f4526c3bfa05b05534db963f8
RemBullis <remington.bullis@mines.sdsmt.edu>
2018-10-04 03:52:56 -0600

    Changed the update_service script a bit to have an explicit path.

--- bringup/update_service.sh
+++ bringup/update_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl stop geekbot_bringup.service
 rm -rf /etc/systemd/system/geekbot_bringup.service
-link geekbot_bringup.service /etc/systemd/system/geekbot_bringup.service
+link /root/geekbot_ws/src/geekbot_pkg/bringup/geekbot_bringup.service /etc/systemd/system/geekbot_bringup.service
 systemctl enable /etc/systemd/system/geekbot_bringup.service
 systemctl daemon-reload
 systemctl start geekbot_bringup.service

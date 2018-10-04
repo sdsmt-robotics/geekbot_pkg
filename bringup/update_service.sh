2fe15f7c29b7a4a9309ed8ca7d397f74feaa96f8
RemBullis <remington.bullis@mines.sdsmt.edu>
2018-10-04 02:02:21 -0600

    Updated service script to stop->reinstall->restart service on update.

--- bringup/update_service.sh
+++ bringup/update_service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+systemctl stop geekbot_bringup.service
 rm -rf /etc/systemd/system/geekbot_bringup.service
 link geekbot_bringup.service /etc/systemd/system/geekbot_bringup.service
 systemctl enable /etc/systemd/system/geekbot_bringup.service
 systemctl daemon-reload
+systemctl start geekbot_bringup.service
